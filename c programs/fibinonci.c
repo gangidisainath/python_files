2fc11a9dd7f826f6bba14c60ba0d786ddc052035
gangidisainath <120488280+gangidisainath@users.noreply.github.com>
2023-01-13 20:22:10 +0530

    Update fibinonci.c

--- c programs/fibinonci.c
+++ c programs/fibinonci.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 int main() {
 int i,n,f1=0,f2=1,f3;
