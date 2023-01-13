817c5b11f2edac216eaf66632e5e24ebb017a28b
gangidisainath <120488280+gangidisainath@users.noreply.github.com>
2023-01-13 20:13:47 +0530

    Update multiplication tables.py.py

--- python programs/multiplication tables.py.py
+++ python programs/multiplication tables.py.py
@@ -1,11 +1,4 @@
-'''
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-'''
 a =int(input())
 for i in range(1,abs(a)+1):
   print(a,"*",i,"=",a*i)
